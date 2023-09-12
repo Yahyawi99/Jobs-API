8c128f3ccc7512ce6fca41ab000cbe6e4fda8924
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-12 17:30:36 +0100

    project setup

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
 const app = express();
 
+// DB
+const connect = require("./db/connect");
+
+// Routes
+const AuthRouter = require("./routes/auth");
+const JobRouter = require("./routes/jobs");
+
 // error handler
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/jobs", JobRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +29,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    // mongo
+    await connect(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
---
+++ controllers/jobs.js
@@ -0,0 +1,27 @@
+const getAllJobs = async (req, res) => {
+  res.send("register");
+};
+
+const getSingleJob = async (req, res) => {
+  res.send("login");
+};
+
+const createJob = async (req, res) => {
+  res.send("login");
+};
+
+const updateJob = async (req, res) => {
+  res.send("login");
+};
+
+const deleteJob = async (req, res) => {
+  res.send("login");
+};
+
+module.exports = {
+  getAllJobs,
+  getSingleJob,
+  createJob,
+  updateJob,
+  deleteJob,
+};
