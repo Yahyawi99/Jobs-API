a3825c5fff679d8c9a194429baacba60f2ac0c05
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-12 18:12:13 +0100

    hash password

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = mongoose.Schema({
   name: {
@@ -24,6 +25,11 @@ const UserSchema = mongoose.Schema({
     required: [true, "Please provide a password"],
     minlength: 8,
   },
+});
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", UserSchema);
