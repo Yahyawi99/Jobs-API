be9e66db6b66e4824c0a69615e109ca7eb429681
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-13 18:49:23 +0100

    validation errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -8,15 +8,22 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   };
 
   if (err.name === "ValidationError") {
-    customError.msg = Object.values(err.errors).map((item) => item.message);
-    customError.statusCode = 400;
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No job found with id : ${err.value}`;
+    customError.statusCode = StatusCodes.NOT_FOUND;
   }
 
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for the ${
       Object.keys(err.keyValue)[0]
     } field, please enter another value.`;
-    customError.statusCode = 400;
+    customError.statusCode = StatusCodes.BAD_REQUEST;
   }
 
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
