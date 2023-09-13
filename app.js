ce0fa905a08491ebde5e181dce01f472bff91f80
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-13 18:55:52 +0100

    security

--- app.js
+++ app.js
@@ -18,8 +18,22 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-app.use(express.json());
 // extra packages
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+const xss = require("xss-clean");
+
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 
 // routes
 app.use("/api/v1/auth", AuthRouter);
