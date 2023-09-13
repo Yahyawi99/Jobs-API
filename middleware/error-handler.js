dd8ffb8f515860e6b6f181de494f409aa24f9123
Yahyawi99 <yassinyahyawi26@gmail.com>
2023-09-13 18:41:53 +0100

    validation errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,6 +7,11 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later!",
   };
 
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors).map((item) => item.message);
+    customError.statusCode = 400;
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for the ${
       Object.keys(err.keyValue)[0]
@@ -14,6 +19,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     customError.statusCode = 400;
   }
 
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ message: customError.msg });
 };
 
